4cd0ad47c99b39bbfc65fe4ea97f6c2fdfec2980
JoeYashinsky <joey.yashinsky@gmail.com>
2020-12-02 12:31:51 -0500

    Created an ISweepstakesManager interface. Created a SweepstakesQueueManager class and SweepstakesStackManager class, both of which will inherit from the ISweepstakesManager interface. Included both methods from ISweepstakesManager in the classes that inherit from this interface: the InsertSweepstakes method and GetSweepstakes method

---
+++ Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    public class Sweepstakes
+    {
+
+
+
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+
+        public Contestant PickWinner()
+        {
+
+        }
+
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+    }
+}
