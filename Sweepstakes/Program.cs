0a0d727c4c4126b7a6ce84a71bcb8ab3e2979ce2
JoeYashinsky <joey.yashinsky@gmail.com>
2020-12-02 12:06:16 -0500

    Created the Customer class and included member variables such as firstName, lastName, emailAddress, and registrationNumber

--- Sweepstakes/Program.cs
+++ Sweepstakes/Program.cs
@@ -10,6 +10,13 @@ namespace Sweepstakes
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
+
+//You are part of a team working on an application that provides the functionality to manage many sweepstakes.
+//Marketing firms are likely purchasers and users of the backend application.
+//They can choose to use your functionality however they like.
+//It is up to you, the developer, to create the backend application to implement the logic for the functions
+// and pieces of functionality in these User Stories.
