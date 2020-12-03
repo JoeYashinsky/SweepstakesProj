9374b1f71cc6e88f291424f0fc8b1060ce0f3dcc
JoeYashinsky <joey.yashinsky@gmail.com>
2020-12-03 10:03:30 -0500

    Implemented methods in SweepstakesStackManager class that were inherited from ISweepStakesManager interface

--- Sweepstakes/SweepstakesQueueManager.cs
+++ Sweepstakes/SweepstakesQueueManager.cs
@@ -23,4 +23,13 @@ namespace Sweepstakes
     }
 }
 
-//Create a SweepstakesQueueManager class that uses the Queue data structure as an underlying structure
+//Create a SweepstakesQueueManager class that uses the Queue data structure as an underlying 
+
+//Queue is an abstract data type that contains elements in linear order
+//Abstract data type is a model of a certain kind of data structure (stack, queue) where behavior
+// is defined rather than implemented, specifically in terms of operations (push, pop) (enqueue, dequeue)
+//Stack and Queue operations are allowed only at the ends, not in the middle
+
+// (FIFO) = First In, First Out   (like people at DMV)
+// Insertion (enqueue) is performed on one end (rear) of the queue while 
+// Deletion (dequeue) is performed on the other end (front).
--- Sweepstakes/SweepstakesStackManager.cs
+++ Sweepstakes/SweepstakesStackManager.cs
@@ -13,14 +13,25 @@ namespace Sweepstakes
         
         public void InsertSweepstakes(Sweepstakes sweepstakes)
         {
-
+            stack.Push(sweepstakes);
         }
 
         public Sweepstakes GetSweepstakes()
         {
-
+            return stack.Pop();
         }
     }
 }
 
 //Create a SweepstakesStackManager class that uses the Stack data structure as an underlying structure
+
+//Queue is an abstract data type that contains elements in linear order
+//Abstract data type is a model of a certain kind of data structure (stack, queue) where behavior
+// is defined rather than implemented, specifically in terms of operations (push, pop) (enqueue, dequeue)
+//Stack and Queue operations are allowed only at the ends, not in the middle
+
+// (LIFO) = Last In, First Out  (like a stack of papers)
+// Collection of elements where we can insert (PUSH) or delete (POP) elements only at TOP of stack
+// Cannot push an element onto stack if stack is full (overflow state). 
+// Also cannot pop an element from empty stack (underflow state).
+
